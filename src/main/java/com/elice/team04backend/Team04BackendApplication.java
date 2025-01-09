70ffc79bc3f6cb2e534a84e952ab9e776eeaf9b7
정태승 <hi563@g.skku.edu>
2025-01-09 17:14:18 +0900

    init: first commit

---
+++ src/main/java/com/elice/team04backend/Team04BackendApplication.java
@@ -0,0 +1,13 @@
+package com.elice.team04backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team04BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team04BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elice/team04backend/Team04BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.team04backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team04BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
