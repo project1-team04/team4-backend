946416d103ae425097bfad12a357a62e0961ade4
명광호 <rhkdgh930@naver.com>
2025-01-13 11:25:17 +0900

    feat: projectService 메서드 추가

--- src/main/java/com/elice/team04backend/service/Impl/ProjectServiceImpl.java
+++ src/main/java/com/elice/team04backend/service/Impl/ProjectServiceImpl.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -17,19 +19,59 @@ public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
 
-    //@Preauthorize 사용하면 role이 MEMBER인 유저만 사용하게 가능 예외처리 안해도됨
+    /*
+    @Preauthorize 사용하면 role이 MEMBER인 유저만 사용하게 가능 예외처리 안해도됨
+    CustomException 적용해야해서 임시 예외 설정
+     */
     @Override
-    public ProjectResponseDto postProject(ProjectRequestDto projectRequestDto) {
-        Project project = projectRequestDto.from();
-        Project createProject = projectRepository.save(project);
-        return createProject.from();
+    public ProjectResponseDto getProject(Long projectId) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new IllegalArgumentException("프로젝트를 찾을 수 없습니다. ID: " + projectId));
+        return project.from();
     }
 
     @Override
-    public void patchProject(ProjectUpdateDto projectUpdateDto) {
+    public List<ProjectResponseDto> getAllProjects() {
+        return projectRepository.findAll()
+                .stream()
+                .map(Project::from)
+                .toList();
+    }
+
+    @Override
+    public ProjectResponseDto postProject(ProjectRequestDto projectRequestDto) {
+        String projectKey = generatedProjectKey(projectRequestDto);
+        Project project = projectRequestDto.from(projectKey);
+        Project savedProject = projectRepository.save(project);
+        return savedProject.from();
+    }
+
+    private String generatedProjectKey(ProjectRequestDto projectRequestDto) {
+        StringBuilder sb = new StringBuilder();
+        String[] s = projectRequestDto.getName().toUpperCase().split(" ");
+        for (String value : s) {
+            char input = value.charAt(0);
+            if ('0' <= input && input <= '9') {
+                continue;
+            }
+            sb.append(input);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public ProjectResponseDto patchProject(ProjectUpdateDto projectUpdateDto) {
         Project project = projectRepository.findById(projectUpdateDto.getId())
-                .orElseThrow(() -> new IllegalArgumentException("임시예외"));
-        project.update(projectUpdateDto);
+                .orElseThrow(() -> new IllegalArgumentException("프로젝트를 찾을 수 없습니다. ID: " + projectUpdateDto.getId()));
+        project.updateName(projectUpdateDto);
         Project updateProject = projectRepository.save(project);
+        return updateProject.from();
+    }
+
+    @Override
+    public void deleteProject(Long projectId) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new IllegalArgumentException("프로젝트를 찾을 수 없습니다. ID: " + projectId));
+        projectRepository.delete(project);
     }
 }
--- src/main/java/com/elice/team04backend/service/ProjectService.java
+++ src/main/java/com/elice/team04backend/service/ProjectService.java
@@ -4,7 +4,12 @@ import com.elice.team04backend.dto.Project.ProjectRequestDto;
 import com.elice.team04backend.dto.Project.ProjectResponseDto;
 import com.elice.team04backend.dto.Project.ProjectUpdateDto;
 
+import java.util.List;
+
 public interface ProjectService {
+    ProjectResponseDto getProject(Long projectId);
+    List<ProjectResponseDto> getAllProjects();
     ProjectResponseDto postProject(ProjectRequestDto projectRequestDto);
-    void patchProject(ProjectUpdateDto projectUpdateDto);
+    ProjectResponseDto patchProject(ProjectUpdateDto projectUpdateDto);
+    void deleteProject(Long projectId);
 }
