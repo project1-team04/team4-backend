d241c4644bfc6c98dcb0978496f873a6fe29ead0
LeeTaeJung <nerostarin@naver.com>
2025-01-16 16:24:13 +0900

    fix: .gitlab-ci.yml & deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 서버로 최신 빌드 파일 복사 (예: /home/gitlab-runner/app 디렉토리로)
-cp build/libs/team04-backend-0.0.1-SNAPSHOT.jar /home/kdt/team04-backend/app/demo.jar
+cp build/libs/team04-backend-0.0.1-SNAPSHOT.jar /home/gitlab-runner/app/demo.jar
 
 # 서버에서 Spring Boot 애플리케이션 실행
 # 기존에 실행 중인 프로세스가 있다면 종료
