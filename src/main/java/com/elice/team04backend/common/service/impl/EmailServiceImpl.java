067d95a2b3d6269496303d4ba6acf6be60e70614
정태승 <hi563@g.skku.edu>
2025-01-16 21:13:21 +0900

    feat: 이메일 전송 기능 및 인증코드 생성 기능 추가
    - Email Server는 Gmail를 이용 (별도의 application 설정 필요)
    - SecureRandom 의존성을 이용해 안전한 6자리 인증코드 생성

---
+++ src/main/java/com/elice/team04backend/common/service/impl/EmailServiceImpl.java
@@ -0,0 +1,25 @@
+package com.elice.team04backend.common.service.impl;
+
+import com.elice.team04backend.common.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        message.setFrom("threadly@elice.com");
+
+        javaMailSender.send(message);
+    }
+
+}
