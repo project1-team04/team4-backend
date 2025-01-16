85ab081ac9748889c9c56fd0815ac206419e6b89
명광호 <rhkdgh930@naver.com>
2025-01-16 22:58:01 +0900

    feat: issueImage 관련 dto 추가

---
+++ src/main/java/com/elice/team04backend/dto/issueImage/IssueImageResponseDto.java
@@ -0,0 +1,13 @@
+package com.elice.team04backend.dto.issueImage;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class IssueImageResponseDto {
+    private Long id;
+    private String imageUrl;
+}
