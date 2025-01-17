bf4cb9bdaf7955048bfc5a3ded8ffe562f58f1c3
LeeTaeJung <nerostarin@naver.com>
2025-01-17 13:26:34 +0900

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ cp build/libs/team04-backend-0.0.1-SNAPSHOT.jar /home/gitlab-runner/app/demo.jar
 pkill -f 'java -jar'
 
 # 새로 빌드한 jar 파일로 애플리케이션 실행
-nohup java -jar /home/gitlab-runner/app/demo.jar &
+nohup java -jar /home/kdt/builds/fwscY_jC/0/pttrack/class_01/web_project_i/team04/team04-backend/build/libs &
 
 echo "Deployment completed successfully!"
