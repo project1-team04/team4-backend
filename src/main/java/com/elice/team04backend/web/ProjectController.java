ac6f37f483895a1bb3231aa4a0a40e6fccb51e4a
명광호 <rhkdgh930@naver.com>
2025-01-18 04:07:54 +0900

    feat: 프로젝트 관련 swagger문서 작성

--- src/main/java/com/elice/team04backend/web/ProjectController.java
+++ src/main/java/com/elice/team04backend/web/ProjectController.java
@@ -9,6 +9,8 @@ import com.elice.team04backend.dto.project.ProjectResponseDto;
 import com.elice.team04backend.dto.project.ProjectUpdateDto;
 import com.elice.team04backend.service.LabelService;
 import com.elice.team04backend.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,23 +26,23 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/api/projects")
 @RequiredArgsConstructor
+@Tag(name = "Project 관리", description = "Project 관련 API 입니다.")
 public class ProjectController {
 
     private final ProjectService projectService;
     private final LabelService labelService;
 
-    /**
-     * TODO
-     * 유저 아이디로 project 찾는 메서드 추가
-     */
 
     @PreAuthorize("hasRole('MANAGER')")
     @GetMapping("/test")
-    public ResponseEntity testProject(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam Long projectId) {
+    public ResponseEntity testProject(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam Long projectId) {
         log.info("{}",userDetails.getUserId());
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "로그인 유저와 관련된 프로젝트 조회", description = "로그인 유저와 관련된 모든 프로젝트를 조회합니다.")
     @GetMapping
     public ResponseEntity<List<ProjectResponseDto>> getUserProjects(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -50,6 +52,7 @@ public class ProjectController {
         return ResponseEntity.ok(projectResponseDtos);
     }
 
+    @Operation(summary = "프로젝트 작성", description = "프로젝트를 작성합니다.")
     @PostMapping
     public ResponseEntity<ProjectResponseDto> postProject(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -58,18 +61,18 @@ public class ProjectController {
         return ResponseEntity.status(HttpStatus.CREATED).body(projectResponseDto);
     }
 
-    @PreAuthorize("hasRole('MANAGER')")
+    @Operation(summary = "단일 프로젝트 조회", description = "프로젝트 id로 단일 프로젝트를 조회합니다.")
     @GetMapping("/{projectId}")
-    public ResponseEntity<ProjectResponseDto> getProject(
+    public ResponseEntity<ProjectResponseDto> getProjectDetails(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @PathVariable Long projectId) {
         ProjectResponseDto projectResponseDto = projectService.getProjectById(projectId);
         return ResponseEntity.ok(projectResponseDto);
     }
 
+    @Operation(summary = "단일 프로젝트 수정", description = "단일 프로젝트를 수정합니다.")
     @PreAuthorize("hasRole('MANAGER')")
     @PatchMapping
-    //@PatchMapping("/{projectId}")
     public ResponseEntity<ProjectResponseDto> patchProject(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestParam Long projectId,
@@ -78,6 +81,7 @@ public class ProjectController {
         return ResponseEntity.ok(projectResponseDto);
     }
 
+    @Operation(summary = "단일 프로젝트 삭제", description = "단일 프로젝트를 삭제합니다.")
     @PreAuthorize("hasRole('MANAGER')")
     @DeleteMapping
     public ResponseEntity<Void> deleteProject(
@@ -86,14 +90,6 @@ public class ProjectController {
         projectService.deleteProject(userDetails.getUserId(), projectId);
         return ResponseEntity.noContent().build();
     }
-
-
-    //    @GetMapping
-//    public ResponseEntity<List<ProjectResponseDto>> getAllProjects() {
-//        List<ProjectResponseDto> projectResponseDtos = projectService.getAllProjects();
-//        return ResponseEntity.ok(projectResponseDtos);
-//    }
-
 
     //라벨 관련 컨트롤러
 
