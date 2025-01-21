e9fcf65632849a2742bbcfd8583deffbfe55bd8f
명광호 <rhkdgh930@naver.com>
2025-01-21 23:41:36 +0900

    feat: 더미 데이터 추가

--- src/main/java/com/elice/team04backend/service/impl/AuthServiceImpl.java
+++ src/main/java/com/elice/team04backend/service/impl/AuthServiceImpl.java
@@ -152,6 +152,15 @@ public class AuthServiceImpl implements AuthService {
                 .password(passwordEncoder.encode("!a12345678"))
                 .build();
 
+        User user4 = User.builder()
+                .email("mkh9900@naver.com")
+                .username("명광호")
+                .provider(Provider.EMAIL)
+                .status(UserStatus.ACTIVE)
+                .isVerified(true)
+                .password(passwordEncoder.encode("!a12345678"))
+                .build();
+
         Project project = Project.builder()
                 .projectKey("h")
                 .name("hell")
@@ -167,6 +176,7 @@ public class AuthServiceImpl implements AuthService {
         userRepository.save(user);
         userRepository.save(user2);
         userRepository.save(user3);
+        userRepository.save(user4);
         projectRepository.save(project);
         userProjectRoleRepository.save(userProjectRole);
 
